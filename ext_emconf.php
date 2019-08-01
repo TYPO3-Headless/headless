e52b8fe6e555ed25ed5f98735f94ef6a2cb827f1
Łukasz Uznański <l.uznanski@macopedia.pl>
2019-08-01 17:46:34 +0200

    Set email to developer, change version

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,14 +5,14 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'This extension provides way to output content from TYPO3 in JSON format.',
     'state' => 'beta',
     'author' => 'Łukasz Uznański',
-    'author_email' => 'l.uznanski@macopedia.pl',
+    'author_email' => 'extensions@macopedia.pl',
     'category' => 'fe',
     'internal' => '',
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
-            'frontend' => '9.5.0-9.5.99'
+            'typo3' => '9.5.0-10.0.99',
+            'frontend' => '9.5.0-10.0.99'
         ],
         'conflicts' => [],
         'suggests' => [],
