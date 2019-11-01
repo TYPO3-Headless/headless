1b6efe38160347d08f6a1b90b2ca3027f7f1d42f
Łukasz Uznański <l.uznanski@macopedia.pl>
2019-11-01 21:24:09 +0100

    [TASK] Set ext version to 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,12 +3,12 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'headless',
     'description' => 'This extension provides way to output content from TYPO3 in JSON format.',
-    'state' => 'beta',
+    'state' => 'stable',
     'author' => 'Łukasz Uznański',
     'author_email' => 'extensions@macopedia.pl',
     'category' => 'fe',
     'internal' => '',
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.0.99',
