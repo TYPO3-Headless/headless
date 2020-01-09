fdb2e3207eef37c656716c5f18591b43d76019c5
Sven Carstens <sven.carstens@aoe.com>
2020-01-09 14:33:18 +0100

    [TASK] Add testing for CEs html and header.

--- Tests/Functional/ContentTypes/HeaderElementTest.php
+++ Tests/Functional/ContentTypes/HeaderElementTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use FriendsOfTYPO3\Headless\Test\Functional\ContentTypes\BaseTest;
 use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
 
-class TextElementTest extends BaseTest
+class HeaderElementTest extends BaseTest
 {
     public function testTextContentElement()
     {
@@ -17,14 +17,11 @@ class TextElementTest extends BaseTest
 
         $fullTree = json_decode((string)$response->getBody(), true);
 
-        $contentElement = $fullTree['content']['colPos0'][0];
+        $contentElement = $fullTree['content']['colPos0'][1];
 
-        $this->checkDefaultContentFields($contentElement, 1, 1, 'text', 0);
+        $this->checkDefaultContentFields($contentElement, 3, 1, 'header', 0);
         $this->checkAppearanceFields($contentElement, 'layout-1', 'Frame', 'SpaceBefore', 'SpaceAfter');
         $this->checkHeaderFields($contentElement, 'Header', 'SubHeader', 1, 2);
         $this->checkHeaderFieldsLink($contentElement, 't3://page?uid=2 _blank LinkClass LinkTitle parameter=999', 'page', '/page1?parameter=999&cHash=', ' target="_blank"');
-
-        // typolink parser was called on bodytext
-        $this->assertStringContainsString('<a href="/page1?parameter=999&amp;cHash=', $contentElement['content']['bodytext']);
     }
 }
