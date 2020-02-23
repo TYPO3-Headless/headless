62cb29c78fcd02cd601763395bfb8ba78bea225d
Oskar Dydo <o.dydo@macopedia.pl>
2020-02-23 22:57:13 +0100

    Add to site configuration frontendBase url field

---
+++ Configuration/SiteConfiguration/Overrides/site.php
@@ -0,0 +1,23 @@
+<?php
+$tempColumns = [
+    'frontendBase' => [
+        'label' => 'Frontend Entry Point',
+        'description' => 'Main URL to call the frontend in default language.',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim',
+            'placeholder' => 'http://www.domain.local',
+        ],
+    ]
+];
+
+$GLOBALS['SiteConfiguration']['site']['columns']['base']['label'] = 'TYPO3 Entry Point';
+$GLOBALS['SiteConfiguration']['site']['columns']['base']['description'] = 'Main URL to call the TYPO3 headless api in default language.';
+
+$GLOBALS['SiteConfiguration']['site']['columns'] = array_merge($GLOBALS['SiteConfiguration']['site']['columns'], $tempColumns);
+
+$GLOBALS['SiteConfiguration']['site']['palettes']['base']['showitem'] = str_replace(
+    'base,',
+    'base, frontendBase, ',
+    $GLOBALS['SiteConfiguration']['site']['palettes']['base']['showitem']
+);
