dd7d879841b297a7fb88a1c6ffd8c7d560661963
Łukasz Uznański <l.uznanski@macopedia.com>
2020-07-10 15:28:47 +0200

    [FEATURE] Add license data to Form Xclass

--- Classes/XClass/Controller/FormFrontendController.php
+++ Classes/XClass/Controller/FormFrontendController.php
@@ -1,4 +1,16 @@
 <?php
+
+/***
+ *
+ * This file is part of the "headless" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ *  (c) 2019
+ *
+ ***/
+
 declare(strict_types=1);
 
 namespace FriendsOfTYPO3\Headless\XClass\Controller;
