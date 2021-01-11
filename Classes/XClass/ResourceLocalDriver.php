8e87ffc92cdb39bc4e4df9df00ffefa5ba9b8353
Tomasz Woldański <t.woldanski@macopedia.com>
2021-01-11 11:18:14 +0100

    [FIX] fix return type in FrontendBaseUtility - simplify method override in ResourceLocalDriver

--- Classes/XClass/ResourceLocalDriver.php
+++ Classes/XClass/ResourceLocalDriver.php
@@ -14,15 +14,8 @@ declare(strict_types=1);
 namespace FriendsOfTYPO3\Headless\XClass;
 
 use FriendsOfTYPO3\Headless\Utility\FrontendBaseUtility;
-use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\PathUtility;
-
-use function array_map;
-use function explode;
-use function implode;
-use function rtrim;
 
 use const TYPO3_MODE;
 
@@ -39,25 +32,13 @@ class ResourceLocalDriver extends \TYPO3\CMS\Core\Resource\Driver\LocalDriver
         if ($this->hasCapability(ResourceStorage::CAPABILITY_PUBLIC)) {
             $conf = $GLOBALS['TYPO3_REQUEST']->getAttribute('site')->getConfiguration();
             $frontendBase = GeneralUtility::makeInstance(FrontendBaseUtility::class);
-
-            $baseUri = $frontendBase->resolveWithVariants(
+            $this->configuration['baseUri'] = $frontendBase->resolveWithVariants(
                 $conf['frontendFileApi'] ?? '',
                 $conf['baseVariants'] ?? null,
                 'frontendFileApi'
             );
-
-            if (!empty($baseUri)) {
-                $this->baseUri = rtrim($baseUri, '/') . '/';
-            } elseif (GeneralUtility::isFirstPartOfStr($this->absoluteBasePath, Environment::getPublicPath())) {
-                // use site-relative URLs
-                $temporaryBaseUri = rtrim(PathUtility::stripPathSitePrefix($this->absoluteBasePath), '/');
-                if ($temporaryBaseUri !== '') {
-                    $uriParts = explode('/', $temporaryBaseUri);
-                    $uriParts = array_map('rawurlencode', $uriParts);
-                    $temporaryBaseUri = implode('/', $uriParts) . '/';
-                }
-                $this->baseUri = $temporaryBaseUri;
-            }
         }
+
+        parent::determineBaseUrl();
     }
 }
