4c5322e170242cf7f0788036033f6673f0a15dbb
Patryk Miedziaszczyk <pmiedziaszczyk@wp.pl>
2021-07-14 15:40:08 +0200

    [TASK] Functional test for the Menu Recently Updated Pages Element
    Basic test for Menu Recently Updated Pages Element checking basic information.

---
+++ Tests/Functional/ContentTypes/MenuRecentlyUpdatedPagesElementTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the "headless" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * (c) 2021
+ */
+
+declare(strict_types=1);
+
+use FriendsOfTYPO3\Headless\Test\Functional\ContentTypes\BaseContentTypeTest;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
+
+class MenuRecentlyUpdatedPagesElementTest extends BaseContentTypeTest
+{
+    public function testTextContentElement()
+    {
+        $response = $this->executeFrontendRequest(
+            new InternalRequest('https://website.local/')
+        );
+
+        self::assertEquals(200, $response->getStatusCode());
+
+        $fullTree = json_decode((string)$response->getBody(), true);
+
+        $contentElement = $fullTree['content']['colPos1'][7];
+
+        print_r($contentElement);
+
+        $this->checkDefaultContentFields($contentElement, 16, 1, 'menu_recently_updated', 1);
+        $this->checkAppearanceFields($contentElement, 'default', 'default', 'SpaceBefore', 'SpaceAfter');
+        $this->checkHeaderFields($contentElement, 'Header', 'SubHeader', 0, 2);
+
+        self::assertTrue(is_array($contentElement['appearance']));
+        self::assertTrue(is_array($contentElement['content']));
+        self::assertTrue(is_array($contentElement['content']['menu']));
+    }
+}
