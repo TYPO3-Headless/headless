2a65480a4e56eec994e82bd9448270cbef9eac6d
Patryk Miedziaszczyk <pmiedziaszczyk@wp.pl>
2021-07-15 16:40:15 +0200

    [TASK] Functional test for the Menu Pages Element
    Basic test for Menu Pages Element checking basic information.

---
+++ Tests/Functional/ContentTypes/MenuPagesElementTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the "headless" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * (c) 2021
+ */
+
+declare(strict_types=1);
+
+use FriendsOfTYPO3\Headless\Test\Functional\ContentTypes\BaseContentTypeTest;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
+
+class MenuPagesElementTest extends BaseContentTypeTest
+{
+    public function testTextContentElement()
+    {
+        $response = $this->executeFrontendRequest(
+            new InternalRequest('https://website.local/')
+        );
+
+        self::assertEquals(200, $response->getStatusCode());
+
+        $fullTree = json_decode((string)$response->getBody(), true);
+
+        $contentElement = $fullTree['content']['colPos1'][5];
+
+        $this->checkDefaultContentFields($contentElement, 14, 1, 'menu_pages', 1);
+        $this->checkAppearanceFields($contentElement, 'default', 'default', 'SpaceBefore', 'SpaceAfter');
+        $this->checkHeaderFields($contentElement, 'Header', 'SubHeader', 0, 2);
+
+        self::assertTrue(is_array($contentElement['appearance']));
+        self::assertTrue(is_array($contentElement['content']));
+        self::assertTrue(is_array($contentElement['content']['menu']));
+        self::assertTrue(is_array($contentElement['content']['menu'][0]));
+        self::assertTrue(is_array($contentElement['content']['menu'][1]));
+        self::assertTrue(is_array($contentElement['content']['menu'][2]));
+        self::assertTrue(is_array($contentElement['content']['menu'][3]));
+
+        self::assertEquals('Root', $contentElement['content']['menu'][0]['title']);
+        self::assertEquals('/', $contentElement['content']['menu'][0]['link']);
+        self::assertEquals('1', $contentElement['content']['menu'][0]['active']);
+        self::assertEquals('1', $contentElement['content']['menu'][0]['current']);
+        self::assertEquals('0', $contentElement['content']['menu'][0]['spacer']);
+
+        self::assertEquals('Page 1', $contentElement['content']['menu'][1]['title']);
+        self::assertEquals('/page1', $contentElement['content']['menu'][1]['link']);
+        self::assertEquals('0', $contentElement['content']['menu'][1]['active']);
+        self::assertEquals('0', $contentElement['content']['menu'][1]['current']);
+        self::assertEquals('0', $contentElement['content']['menu'][1]['spacer']);
+
+        self::assertEquals('Page 1.1', $contentElement['content']['menu'][2]['title']);
+        self::assertEquals('/page1/page1_1', $contentElement['content']['menu'][2]['link']);
+        self::assertEquals('0', $contentElement['content']['menu'][2]['active']);
+        self::assertEquals('0', $contentElement['content']['menu'][2]['current']);
+        self::assertEquals('0', $contentElement['content']['menu'][2]['spacer']);
+
+        self::assertEquals('Page 2', $contentElement['content']['menu'][3]['title']);
+        self::assertEquals('/page2', $contentElement['content']['menu'][3]['link']);
+        self::assertEquals('0', $contentElement['content']['menu'][3]['active']);
+        self::assertEquals('0', $contentElement['content']['menu'][3]['current']);
+        self::assertEquals('0', $contentElement['content']['menu'][3]['spacer']);
+    }
+}
