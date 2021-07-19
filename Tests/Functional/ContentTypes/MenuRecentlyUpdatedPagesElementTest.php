c869ad70fa13832b4abbbfa5a7a54c283240564f
Patryk Miedziaszczyk <pmiedziaszczyk@wp.pl>
2021-07-19 10:43:01 +0200

    [TASK] Functional test for the Menu Recently Updated Pages Element
    Basic test for Menu Recently Updated Pages Element checking basic information.

--- Tests/Functional/ContentTypes/MenuRecentlyUpdatedPagesElementTest.php
+++ Tests/Functional/ContentTypes/MenuRecentlyUpdatedPagesElementTest.php
@@ -12,10 +12,25 @@
 declare(strict_types=1);
 
 use FriendsOfTYPO3\Headless\Test\Functional\ContentTypes\BaseContentTypeTest;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
 
 class MenuRecentlyUpdatedPagesElementTest extends BaseContentTypeTest
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        $currentDate = new DateTime();
+        $modifiedOneDayAgo = (clone $currentDate)->modify('-1 day');
+        $modifiedSevenDayAgo = (clone $currentDate)->modify('-7 day');
+        $modifiedEightDayAgo = (clone $currentDate)->modify('-8 day');
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('pages');
+        $connection->prepare('update pages set SYS_LASTCHANGED = ? WHERE uid = 2')->execute([$modifiedOneDayAgo->getTimestamp()]);
+        $connection->prepare('update pages set SYS_LASTCHANGED = ? WHERE uid = 3')->execute([$modifiedSevenDayAgo->getTimestamp()]);
+        $connection->prepare('update pages set SYS_LASTCHANGED = ? WHERE uid = 4')->execute([$modifiedEightDayAgo->getTimestamp()]);
+    }
+
     public function testMenuContentElement()
     {
         $response = $this->executeFrontendRequest(
@@ -35,5 +50,23 @@ class MenuRecentlyUpdatedPagesElementTest extends BaseContentTypeTest
         self::assertTrue(is_array($contentElement['appearance']));
         self::assertTrue(is_array($contentElement['content']));
         self::assertTrue(is_array($contentElement['content']['menu']));
+        self::assertTrue(is_array($contentElement['content']['menu'][0]));
+        self::assertTrue(is_array($contentElement['content']['menu'][1]));
+
+        self::assertEquals('Page 1', $contentElement['content']['menu'][0]['title']);
+        self::assertEquals('/page1', $contentElement['content']['menu'][0]['link']);
+        self::assertEquals('0', $contentElement['content']['menu'][0]['active']);
+        self::assertEquals('0', $contentElement['content']['menu'][0]['current']);
+        self::assertEquals('0', $contentElement['content']['menu'][0]['spacer']);
+        self::assertTrue(is_array($contentElement['content']['menu'][0]['media']));
+
+        self::assertEquals('Page 1.1', $contentElement['content']['menu'][1]['title']);
+        self::assertEquals('/page1/page1_1', $contentElement['content']['menu'][1]['link']);
+        self::assertEquals('0', $contentElement['content']['menu'][1]['active']);
+        self::assertEquals('0', $contentElement['content']['menu'][1]['current']);
+        self::assertEquals('0', $contentElement['content']['menu'][1]['spacer']);
+        self::assertTrue(is_array($contentElement['content']['menu'][1]['media']));
+
+        self::assertArrayNotHasKey(2, $contentElement['content']['menu']);
     }
 }
