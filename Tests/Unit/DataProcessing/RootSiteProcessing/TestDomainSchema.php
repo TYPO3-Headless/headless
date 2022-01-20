577d86795659411732b77816cea4b35ed2ed02c7
twoldanski <66474451+twoldanski@users.noreply.github.com>
2022-01-20 17:45:54 +0100

    [TASK] add tests for RootSiteProcessor (#400)

---
+++ Tests/Unit/DataProcessing/RootSiteProcessing/TestDomainSchema.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the "headless" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FriendsOfTYPO3\Headless\Tests\Unit\DataProcessing\RootSiteProcessing;
+
+use FriendsOfTYPO3\Headless\DataProcessing\RootSiteProcessing\SiteProviderInterface;
+use FriendsOfTYPO3\Headless\DataProcessing\RootSiteProcessing\SiteSchemaInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+
+use function str_replace;
+
+class TestDomainSchema implements SiteSchemaInterface
+{
+    /**
+     * @param SiteProviderInterface $provider
+     * @param array<string, mixed> $options
+     * @return array<int, array<string, mixed>>
+     */
+    public function process(SiteProviderInterface $provider, array $options = []): array
+    {
+        $processorConfiguration = $options['processorConfiguration'] ?? [];
+        $cObj = $options['cObj'] ?? GeneralUtility::makeInstance(ContentObjectRenderer::class);
+        $result = [];
+
+        foreach ($provider->getSites() as $site) {
+            $protocol = $site->getBase()->getScheme() . '://';
+            $url = 'https://frontend.tld';
+
+            $locales = [];
+
+            foreach ($site->getLanguages() as $language) {
+                $locales[] = $language->getTypo3Language();
+            }
+
+            $domain = [
+                'name' => str_replace($protocol, '', $url),
+                'baseURL' => $url,
+                'api' => [
+                    'baseURL' => '/proxy/',
+                ],
+                'i18n' => [
+                    'locales' => $locales,
+                    'defaultLocale' => $site->getDefaultLanguage()->getTypo3Language()
+                ],
+            ];
+
+            $result[] = $domain;
+        }
+
+        return $result;
+    }
+}
---
+++ Tests/Unit/DataProcessing/RootSiteProcessing/TestSiteProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the "headless" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FriendsOfTYPO3\Headless\Tests\Unit\DataProcessing\RootSiteProcessing;
+
+use FriendsOfTYPO3\Headless\DataProcessing\RootSiteProcessing\SiteProviderInterface;
+use TYPO3\CMS\Core\Site\Entity\Site;
+
+class TestSiteProvider implements SiteProviderInterface
+{
+    public function prepare(array $config, int $siteUid)
+    {
+    }
+
+    public function getSites(): array
+    {
+        return [new Site('test_site', 1, [ 'base' => 'https://www.typo3.org']), new Site('test_site2', 2, [ 'base' => 'https://forge.typo3.org'])];
+    }
+
+    public function getPages(): array
+    {
+        return [];
+    }
+
+    public function getCurrentRootPage(): Site
+    {
+        return new Site('test_site', 1, []);
+    }
+}
