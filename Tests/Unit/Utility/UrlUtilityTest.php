c5c60ed7f2f1bb9989a1e0261d7fda93ce825f11
twoldanski <66474451+twoldanski@users.noreply.github.com>
2023-03-01 16:37:29 +0100

    [FEATURE] Add possibility to replace FE domains for language variants (#558)
    resolves: #538

--- Tests/Unit/Utility/UrlUtilityTest.php
+++ Tests/Unit/Utility/UrlUtilityTest.php
@@ -20,6 +20,7 @@ use TYPO3\CMS\Core\ExpressionLanguage\Resolver;
 use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Http\Uri;
 use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
@@ -248,6 +249,145 @@ class UrlUtilityTest extends UnitTestCase
         );
     }
 
+    public function testLanguageResolver(): void
+    {
+        $site = $this->prophesize(Site::class);
+        $site->getConfiguration()->shouldBeCalled(2)->willReturn([
+            'base' => 'https://www.typo3.org',
+            'languages' => [],
+            'baseVariants' => [
+                [
+                    'base' => 'https://test-backend-api.tld',
+                    'condition' => 'applicationContext == "Development"',
+                    'frontendBase' => 'https://test-frontend.tld',
+                    'frontendApiProxy' => 'https://test-frontend-api.tld/headless',
+                    'frontendFileApi' => 'https://test-frontend-api.tld/headless/fileadmin'
+                ],
+                [
+                    'base' => 'https://test-second-backend-api.tld',
+                    'condition' => 'applicationContext == "Testing"',
+                    'frontendBase' => 'https://test-second-frontend.tld',
+                    'frontendApiProxy' => 'https://test-second-frontend.tld/headless',
+                    'frontendFileApi' => 'https://test-second-frontend.tld/headless/fileadmin'
+                ]
+            ]
+        ]);
+
+        $resolver = $this->prophesize(Resolver::class);
+        $resolver->evaluate(Argument::containingString('Development'))->willReturn(true);
+        $resolver->evaluate(Argument::containingString('Testing'))->willReturn(false);
+
+        $siteFinder = $this->prophesize(SiteFinder::class);
+
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder->reveal());
+        $urlUtility = $urlUtility->withSite($site->reveal());
+        $urlUtility = $urlUtility->withLanguage(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+            'baseVariants' => [
+                [
+                    'base' => 'https://test-backend-api.tld',
+                    'condition' => 'applicationContext == "Development"',
+                    'frontendBase' => 'https://test-frontend-from-lang.tld',
+                    'frontendApiProxy' => 'https://test-frontend-from-lang.tld/headless',
+                    'frontendFileApi' => 'https://test-frontend-from-lang.tld/headless/fileadmin'
+                ],
+            ],
+        ]));
+
+        // same page, so we make it relative
+        self::assertSame(
+            '/test-page',
+            $urlUtility->prepareRelativeUrlIfPossible('https://test-frontend-from-lang.tld/test-page')
+        );
+
+        self::assertSame(
+            '/test-page?some_query_param=1&some_extra=2',
+            $urlUtility->prepareRelativeUrlIfPossible(
+                'https://test-frontend-from-lang.tld/test-page?some_query_param=1&some_extra=2'
+            )
+        );
+
+        // different domain, so we need absolute url
+        self::assertSame(
+            'https://test-second-frontend.tld/test-page',
+            $urlUtility->prepareRelativeUrlIfPossible('https://test-second-frontend.tld/test-page')
+        );
+
+        self::assertSame('https://test-frontend-from-lang.tld', $urlUtility->getFrontendUrl());
+        self::assertSame('https://test-frontend-from-lang.tld/headless', $urlUtility->getProxyUrl());
+        self::assertSame('https://test-frontend-from-lang.tld/headless/fileadmin', $urlUtility->getStorageProxyUrl());
+
+        // not overlay site variants if language has not defined variants
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder->reveal());
+        $urlUtility = $urlUtility->withSite($site->reveal());
+        $urlUtility = $urlUtility->withLanguage(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+        ]));
+
+        self::assertSame('https://test-frontend.tld', $urlUtility->getFrontendUrl());
+
+        // switch env for language
+        $resolver = $this->prophesize(Resolver::class);
+        $resolver->evaluate(Argument::containingString('Development'))->willReturn(false);
+        $resolver->evaluate(Argument::containingString('Testing'))->willReturn(true);
+
+        $siteFinder = $this->prophesize(SiteFinder::class);
+
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder->reveal());
+        $urlUtility = $urlUtility->withSite($site->reveal());
+        $urlUtility = $urlUtility->withLanguage(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+            'baseVariants' => [
+                [
+                    'base' => 'https://test-backend-api.tld',
+                    'condition' => 'applicationContext == "Development"',
+                    'frontendBase' => 'https://test-frontend-from-lang.tld',
+                    'frontendApiProxy' => 'https://test-frontend-from-lang.tld/headless',
+                    'frontendFileApi' => 'https://test-frontend-from-lang.tld/headless/fileadmin'
+                ],
+                [
+                    'base' => 'https://test-backend-api-testing.tld',
+                    'condition' => 'applicationContext == "Testing"',
+                    'frontendBase' => 'https://test-frontend-from-lang-testing-env.tld',
+                    'frontendApiProxy' => 'https://test-frontend-from-lang-testing-env.tld/headless',
+                    'frontendFileApi' => 'https://test-frontend-from-lang-testing-env.tld/headless/fileadmin'
+                ],
+            ],
+        ]));
+
+        self::assertSame('https://test-frontend-from-lang-testing-env.tld', $urlUtility->getFrontendUrl());
+    }
+
     public function testFrontendUrlForPage(): void
     {
         $site = $this->prophesize(Site::class);
@@ -383,15 +523,16 @@ class UrlUtilityTest extends UnitTestCase
     public function testEdgeCases()
     {
         $site = $this->createMockSite('https://test-backend-api.tld:8000');
-        $request = $this->createPartialMock(ServerRequest::class, ['getAttribute']);
-        $request->method('getAttribute')->willReturn($site);
+        $request = $this->prophesize(ServerRequest::class);
+        $request->getAttribute(Argument::is('site'))->willReturn($site);
+        $request->getAttribute(Argument::is('language'))->willReturn(null);
 
         $resolver = $this->prophesize(Resolver::class);
         $resolver->evaluate(Argument::any())->willReturn(true);
 
         $siteFinder = $this->prophesize(SiteFinder::class);
         $siteFinder->getSiteByPageId(Argument::is(1))->shouldBeCalledOnce()->willReturn($site);
-        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder->reveal(), $request);
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder->reveal(), $request->reveal());
 
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['features']['headless.frontendUrls'] = true;
         self::assertSame(
@@ -401,7 +542,7 @@ class UrlUtilityTest extends UnitTestCase
 
         $siteFinder = $this->createPartialMock(SiteFinder::class, ['getSiteByPageId']);
         $siteFinder->method('getSiteByPageId')->willThrowException(new SiteNotFoundException('test'));
-        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder, $request);
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder, $request->reveal());
 
         self::assertSame(
             'https://test-backend-api.tld:8000/test-page',
@@ -411,11 +552,164 @@ class UrlUtilityTest extends UnitTestCase
         $resolver = $this->createPartialMock(Resolver::class, ['evaluate']);
         $resolver->method('evaluate')->willThrowException(new SyntaxError('test'));
 
-        $urlUtility = new UrlUtility(null, $resolver, $siteFinder, $request);
+        $urlUtility = new UrlUtility(null, $resolver, $siteFinder, $request->reveal());
         self::assertSame('', $urlUtility->getFrontendUrl());
 
         $urlUtility = $urlUtility->withSite($this->createMockSite('https://test-frontend.tld', '', []));
         self::assertSame('', $urlUtility->getFrontendUrl());
+
+        $request = $this->prophesize(ServerRequest::class);
+        $request->getAttribute(Argument::is('site'))->willReturn($site);
+        $request->getAttribute(Argument::is('language'))->willReturn(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+            'baseVariants' => [
+                [
+                    'base' => 'https://test-backend-api.tld',
+                    'condition' => 'applicationContext == "Development"',
+                    'frontendBase' => 'https://test-frontend-from-lang.tld',
+                    'frontendApiProxy' => 'https://test-frontend-from-lang.tld/headless',
+                    'frontendFileApi' => 'https://test-frontend-from-lang.tld/headless/fileadmin'
+                ],
+            ],
+        ]));
+
+        $resolver = $this->prophesize(Resolver::class);
+        $resolver->evaluate(Argument::any())->willReturn(true);
+
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder, $request->reveal());
+        self::assertSame('https://test-frontend-from-lang.tld', $urlUtility->getFrontendUrl());
+
+        $request = $this->prophesize(ServerRequest::class);
+        $request->getAttribute(Argument::is('site'))->willReturn($site);
+        $request->getAttribute(Argument::is('language'))->willReturn(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+        ]));
+
+        $resolver = $this->prophesize(Resolver::class);
+        $resolver->evaluate(Argument::any())->willReturn(true);
+
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder, $request->reveal());
+        self::assertSame('', $urlUtility->getFrontendUrl());
+
+        // configuration on language lvl without variants
+        $request = $this->prophesize(ServerRequest::class);
+        $request->getAttribute(Argument::is('site'))->willReturn($this->createMockSite('https://test-backend-api.tld:8000', '', []));
+        $request->getAttribute(Argument::is('language'))->willReturn(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+            'frontendBase' => 'https://frontend-domain-from-lang.tld',
+            'frontendApiProxy' => 'https://frontend-domain-from-lang.tld/headless/',
+            'frontendFileApi' => 'https://frontend-domain-from-lang.tld/headless/fileadmin',
+        ]));
+
+        $resolver = $this->prophesize(Resolver::class);
+        $resolver->evaluate(Argument::any())->willReturn(true);
+
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder, $request->reveal());
+        self::assertSame('https://frontend-domain-from-lang.tld', $urlUtility->getFrontendUrl());
+        self::assertSame('https://frontend-domain-from-lang.tld/headless/', $urlUtility->getProxyUrl());
+        self::assertSame('https://frontend-domain-from-lang.tld/headless/fileadmin', $urlUtility->getStorageProxyUrl());
+
+        // configuration on language lvl with variants
+
+        $request = $this->prophesize(ServerRequest::class);
+        $request->getAttribute(Argument::is('site'))->willReturn($this->createMockSite('https://test-backend-api.tld:8000', '', []));
+        $request->getAttribute(Argument::is('language'))->willReturn(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+            'frontendBase' => 'https://frontend-domain-from-lang.tld',
+            'frontendApiProxy' => 'https://frontend-domain-from-lang.tld/headless/',
+            'frontendFileApi' => 'https://frontend-domain-from-lang.tld/headless/fileadmin',
+            'baseVariants' => [
+                [
+                    'base' => 'https://test-backend-api.tld',
+                    'condition' => 'applicationContext == "Development"',
+                    'frontendBase' => 'https://test-frontend-from-when-develop-lang.tld',
+                    'frontendApiProxy' => 'https://test-frontend-from-when-develop-lang.tld/headless',
+                    'frontendFileApi' => 'https://test-frontend-from-when-develop-lang.tld/headless/fileadmin'
+                ],
+            ],
+        ]));
+
+        $resolver = $this->prophesize(Resolver::class);
+        $resolver->evaluate(Argument::any())->willReturn(true);
+
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder, $request->reveal());
+        self::assertSame('https://test-frontend-from-when-develop-lang.tld', $urlUtility->getFrontendUrl());
+        self::assertSame('https://test-frontend-from-when-develop-lang.tld/headless', $urlUtility->getProxyUrl());
+        self::assertSame('https://test-frontend-from-when-develop-lang.tld/headless/fileadmin', $urlUtility->getStorageProxyUrl());
+
+        $manualRequest = $this->prophesize(ServerRequest::class);
+        $manualRequest->getAttribute(Argument::is('site'))->willReturn($this->createMockSite('https://test-backend-api.tld:8000', '', []));
+        $manualRequest->getAttribute(Argument::is('language'))->willReturn(new SiteLanguage(0, 'en', new Uri('/'), [
+            'title' =>  'English',
+            'enabled' =>  true,
+            'languageId' =>  0,
+            'base' => ' /',
+            'typo3Language' =>  'default',
+            'locale' =>  'en_US.UTF-8',
+            'iso-639-1'=>  'en',
+            'navigationTitle' =>  'English',
+            'hreflang' => 'en-us',
+            'direction' =>  'ltr',
+            'flag' =>  'us',
+            'frontendBase' => 'https://frontend-domain-from-lang.tld',
+            'frontendApiProxy' => 'https://frontend-domain-from-lang.tld/headless/',
+            'frontendFileApi' => 'https://frontend-domain-from-lang.tld/headless/fileadmin',
+            'baseVariants' => [
+                [
+                    'base' => 'https://test-backend-api.tld',
+                    'condition' => 'applicationContext == "Development"',
+                    'frontendBase' => 'https://test-frontend-from-from-request-lang.tld',
+                    'frontendApiProxy' => 'https://test-frontend-from-from-request-lang.tld/headless',
+                    'frontendFileApi' => 'https://test-frontend-from-from-request-lang.tld/headless/fileadmin'
+                ],
+            ],
+        ]));
+
+        $urlUtility = new UrlUtility(null, $resolver->reveal(), $siteFinder);
+        $urlUtilityWithRequest = $urlUtility->withRequest($manualRequest->reveal());
+        self::assertSame('https://test-frontend-from-from-request-lang.tld', $urlUtilityWithRequest->getFrontendUrl());
+        self::assertSame('https://test-frontend-from-from-request-lang.tld/headless', $urlUtilityWithRequest->getProxyUrl());
+        self::assertSame('https://test-frontend-from-from-request-lang.tld/headless/fileadmin', $urlUtilityWithRequest->getStorageProxyUrl());
     }
 
     protected function createMockSite(string $backendUrl, string $frontendUrl = '', ?array $variants = null)
