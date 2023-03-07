ee6273e3d1b578a9fe338f1a5ae63adc9b247af5
Tomasz Woldański <t.woldanski@macopedia.com>
2023-03-07 18:28:18 +0100

    [TASK] Migrate to AfterPagePreviewUriGeneratedEvent resolves: #489

---
+++ Tests/Unit/Event/Listener/AfterPagePreviewUriGeneratedListenerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the "headless" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+namespace FriendsOfTYPO3\Headless\Event\Listener;
+
+use FriendsOfTYPO3\Headless\Utility\UrlUtility;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use TYPO3\CMS\Core\ExpressionLanguage\Resolver;
+use TYPO3\CMS\Core\Site\SiteFinder;
+
+class AfterPagePreviewUriGeneratedListenerTest extends TestCase
+{
+    use ProphecyTrait;
+
+    public function test__construct()
+    {
+        $resolver = $this->prophesize(Resolver::class);
+        $resolver->evaluate(Argument::any())->willReturn(true);
+        $siteFinder = $this->prophesize(SiteFinder::class);
+
+        $listener = new AfterPagePreviewUriGeneratedListener(new UrlUtility(null, $resolver->reveal(), $siteFinder->reveal()));
+
+        self::assertInstanceOf(AfterPagePreviewUriGeneratedListener::class, $listener);
+    }
+}
