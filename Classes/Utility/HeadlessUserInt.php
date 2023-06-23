4eabfaa10af88506ce8d0319a6f28a6414a835b4
twoldanski <66474451+twoldanski@users.noreply.github.com>
2023-06-23 13:36:17 +0200

    [FEATURE] Allow non cachable plugins to return null on empty (#594)
    Handles cases when plugin marked as USER_INT wants to in some case do not return some data, by default instead of empty string, we want set null for better type system in FE app

--- Classes/Utility/HeadlessUserInt.php
+++ Classes/Utility/HeadlessUserInt.php
@@ -17,42 +17,15 @@ use function preg_quote;
 use function preg_replace;
 use function preg_replace_callback;
 use function sprintf;
-use function strpos;
 use function substr;
 
 class HeadlessUserInt
 {
     public const STANDARD = 'HEADLESS_INT';
     public const NESTED = 'NESTED_HEADLESS_INT';
+    public const STANDARD_NULLABLE = 'HEADLESS_INT_NULL';
+    public const NESTED_NULLABLE = 'NESTED_HEADLESS_INT_NULL';
     private const REGEX = '/("|)%s_START<<(.*?)>>%s_END("|)/s';
-
-    /**
-     * for use in preg_replace_callback
-     * to unwrap all HEADLESS_INT<<>>HEADLESS_INT blocks
-     *
-     * @param array<int, string> $input
-     * @return string
-     */
-    private function replace(array $input): string
-    {
-        $content = $input[2];
-        if ($input[1] === $input[3] && $input[1] === '"') {
-            // have a look inside if it might be json already
-            $decoded = json_decode($content);
-
-            if ($decoded !== null) {
-                return $content;
-            }
-            return json_encode($content);
-        }
-
-        // trim one occurrence of double quotes at both ends
-        $jsonEncoded = json_encode($content);
-        if ($jsonEncoded[0] === '"' && $jsonEncoded[-1] === '"') {
-            $jsonEncoded = substr($jsonEncoded, 1, -1);
-        }
-        return $jsonEncoded;
-    }
 
     public function wrap(string $content, string $type = self::STANDARD): string
     {
@@ -73,10 +46,61 @@ class HeadlessUserInt
             );
         }
 
+        if (strpos($content, self::NESTED_NULLABLE) !== false) {
+            $content = preg_replace_callback(
+                sprintf(self::REGEX, self::NESTED_NULLABLE, self::NESTED_NULLABLE),
+                function (array $content) {
+                    return $this->replace($content, true);
+                },
+                $content
+            );
+        }
+
+        if (strpos($content, self::STANDARD_NULLABLE) !== false) {
+            $content = preg_replace_callback(
+                sprintf(self::REGEX, self::STANDARD_NULLABLE, self::STANDARD_NULLABLE),
+                function (array $content) {
+                    return $this->replace($content, true);
+                },
+                $content
+            );
+        }
+
         return preg_replace_callback(
             sprintf(self::REGEX, self::STANDARD, self::STANDARD),
             [$this, 'replace'],
             $content
         );
+    }
+
+    /**
+     * for use in preg_replace_callback
+     * to unwrap all HEADLESS_INT<<>>HEADLESS_INT blocks
+     *
+     * @param array<int, string> $input
+     */
+    private function replace(array $input, bool $returnNull = false): ?string
+    {
+        $content = $input[2];
+        if ($input[1] === $input[3] && $input[1] === '"') {
+            // have a look inside if it might be json already
+            $decoded = json_decode($content);
+
+            if (empty($decoded) && $returnNull) {
+                return json_encode(null);
+            }
+
+            if ($decoded !== null) {
+                return $content;
+            }
+            return json_encode($content);
+        }
+
+        // trim one occurrence of double quotes at both ends
+        $jsonEncoded = json_encode($content);
+        if ($jsonEncoded[0] === '"' && $jsonEncoded[-1] === '"') {
+            $jsonEncoded = substr($jsonEncoded, 1, -1);
+        }
+        return $jsonEncoded;
     }
 }
