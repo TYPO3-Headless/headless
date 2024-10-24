67d656eaf1103e504e57dd3887a6d7fe0eb56d73
Tomasz Woldański <t.woldanski@macopedia.com>
2024-10-24 10:51:16 +0200

    [TASK] Introduce HeadlessVersion TYPO3's Core inspired simple utility to provide small helper to tackle code adjusting for different versions of ext:headless

---
+++ Classes/Utility/HeadlessVersion.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the "headless" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FriendsOfTYPO3\Headless\Utility;
+
+/**
+ * @codeCoverageIgnore
+ */
+class HeadlessVersion
+{
+    protected const VERSION = '4.5.0';
+
+    public function getVersion(): string
+    {
+        return static::VERSION;
+    }
+
+    public function getMajorVersion(): int
+    {
+        [$explodedVersion] = explode('.', static::VERSION);
+        return (int)$explodedVersion;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getVersion();
+    }
+}
