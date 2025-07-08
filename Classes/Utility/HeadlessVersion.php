cf534f13e51d2c1269099e30e5cef894af878ed8
Tomasz Woldański <t.woldanski@macopedia.com>
2025-07-08 15:22:35 +0200

    [TASK] Set version 4.7

--- Classes/Utility/HeadlessVersion.php
+++ Classes/Utility/HeadlessVersion.php
@@ -16,7 +16,7 @@ namespace FriendsOfTYPO3\Headless\Utility;
  */
 class HeadlessVersion
 {
-    protected const VERSION = '4.6.0';
+    protected const VERSION = '4.7.0';
 
     public function getVersion(): string
     {
